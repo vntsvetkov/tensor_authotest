4c5915a854eee4af8b420a3cfdbe6aa194d59f77
nv.tsvetkov <nv.cvetkov@tensor.ru>
2023-05-15 13:23:28 +0300

    Добавил решение по 5-му вебинару

---
+++ Webinar5/task_with_star.py
@@ -0,0 +1,47 @@
+# Напишите функцию to_roman, которая преобразуют арабское число (val) в римское (roman_str).
+#
+# Современные римские цифры записываются, выражая каждую цифру отдельно,
+# начиная с самой левой цифры и пропуская цифру со значением нуля.
+# Римскими цифрами 1990 отображается: 1000=М, 900=СМ, 90=ХС; в результате MCMXC.
+# 2023 записывается как 2000=MM, 20=XX, 3=III; или MMXXIII.
+# В 1666 используется каждый римский символ в порядке убывания: MDCLXVI.
+#
+# Например (Ввод --> Вывод) :
+# 2008 --> MMVIII
+
+
+def to_roman(val):
+    roman_str = ''
+    roman_dict = {1000: 'M',
+                  900: 'CM',
+                  500: 'D',
+                  400: 'CD',
+                  100: 'C',
+                  90: 'XC',
+                  50: 'L',
+                  40: 'XL',
+                  10: 'X',
+                  9: 'IX',
+                  5: 'V',
+                  4: 'IV',
+                  1: 'I'}
+    for arabic, roman in roman_dict.items():
+        if val // arabic:
+            roman_str += (val // arabic) * roman
+            val %= arabic
+    return roman_str
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+data = [1133, 2224, 1938, 1817, 2505, 391, 3743, 1634, 699, 1666, 1494, 1444]
+
+test_data = [
+    "MCXXXIII", "MMCCXXIV", "MCMXXXVIII", "MDCCCXVII", "MMDV", "CCCXCI", 'MMMDCCXLIII', 'MDCXXXIV', 'DCXCIX', 'MDCLXVI',
+    'MCDXCIV', 'MCDXLIV']
+
+
+for i, d in enumerate(data):
+    assert to_roman(d) == test_data[i], f'С набором {d} есть ошибка, не проходит проверку'
+    print(f'Тестовый набор {d} прошёл проверку')
+print('Всё ок')
